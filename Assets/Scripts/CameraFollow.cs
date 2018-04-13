6dd4261ad6ac54fee083c14faca437afd72670ef
Yang Liu <arkilis@gmail.com>
2018-04-14 08:30:41 +1000

    init commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform player;
+	private Vector3 offset;
+
+	void Start () {
+		//Calculate and store the offset value by getting the distance between the player's position and camera's position.
+		offset = transform.position - player.transform.position;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = player.transform.position + offset;
+		Debug.Log (transform.position);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public int m_speed = 10;
+	public int m_force = 100;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().velocity = Vector3.forward * m_speed;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if (Input.GetKeyDown ("a")) {
+			Debug.Log ("A");
+			GetComponent<Rigidbody>().AddForce(Vector3.left * m_force);
+		}
+
+		if (Input.GetKeyDown ("d")) {
+			Debug.Log ("D");
+			GetComponent<Rigidbody> ().AddForce (Vector3.right * m_force);
+		}
+		
+	}
+}
